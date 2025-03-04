55da880c1dd3096df4d045c063b5b01ce4ddb089
manel-balsells <manel.balsells@estudiantat.upc.edu>
2025-03-04 09:46:52 +0100

    Update Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,1 +1,19 @@
 #Codigo para problema 1
+#Codigo para problema 1
+#Pregunta 1, trobar R^2 = Multiple R-squared
+# Ens la dona l'enunciat, R = 0.1923
+
+#Pregunta 2: coeficient de correlacion = R
+
+Rsq = 0.1923
+R <- sqrt(Rsq)
+R
+#Pregunta 3, es el pendent, 0,06576
+
+#Pregunta 4, prediccio a 647.1
+
+m <- 0.06576
+m
+b <- 11.79755
+b
+m*647.1+b
